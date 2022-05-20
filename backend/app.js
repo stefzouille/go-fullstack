aa292cd42834bfe67e66e15b7cea3e4589d1a2ff
stefzouille <contact.lokcy@gmail.com>
2022-05-20 13:46:39 +0200

    add

--- backend/app.js
+++ backend/app.js
@@ -20,11 +20,9 @@ const userRoutes = require('./routes/user');
 const mdpApi = process.env.PROJECT_API_KEY;
 
 const nameApi = process.env.NAME_API;
-console.log(nameApi);
 
-// mongodb + srv://<username>:<password>@cluster0.e9dod.mongodb.net/?retryWrites=true&w=majority
 
-var dataBase = `mongodb+srv://${nameApi}:${mdpApi}@cluster0.e9dod.mongodb.net/?retryWrites=true&w=majority`
+const dataBase = `mongodb+srv://${nameApi}:${mdpApi}@cluster0.e9dod.mongodb.net/?retryWrites=true&w=majority`
 
 mongoose.connect(dataBase,
 
